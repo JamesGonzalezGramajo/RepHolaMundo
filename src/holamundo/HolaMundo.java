6f7b8ee47cc2ac973bc4b35d50e7629b32b1e576
JamesGonzalez <jamesanthony-gonzalezgramajo@cunc.edu.gt>
2018-08-01 17:41:11 -0600

    es el nuevo cambio

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -8,6 +8,8 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo desde java");
+        System.out.println("Este es el cambio");
+        
     }
     
 }
