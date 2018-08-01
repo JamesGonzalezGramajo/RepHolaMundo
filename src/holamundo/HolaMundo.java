a9da97ef437f2daed396537769f37b5f6def0f5e
JamesGonzalez <jamesanthony-gonzalezgramajo@cunc.edu.gt>
2018-08-01 17:28:32 -0600

    este es mi comentario putos

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+/**
+ *
+ * @author james
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde java");
+    }
+    
+}
